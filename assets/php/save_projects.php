8854b9b2e07eb0b076c961380642cea8bfa6f0da
notSean84 <sean.hug@icloud.com>
2025-02-11 16:54:04 +0100

    PHP: Save_projects begonnen

---
+++ assets/php/save_projects.php
@@ -0,0 +1,16 @@
+<?php
+include "db.php";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $projectName = $_POST['projectName'];
+    $startDate = $_POST['projectStartDate'];
+    $endDate = $_POST['projectEndDate'];
+// WICHTIG: AUTO INCREMENT MUSS BEI PROJECT UND ROLE NOCHT EINGESTELLT WERDEN
+    $sql = "INSERT INTO project (name, start_date, end_date) VALUES ('$projectName', '$startDate', '$endDate')";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: index.html"); // Weiterleitung zurück zur Startseite
+        exit();
+    } else {
+        echo "Fehler: " . $conn->error;
+    }    
+    $conn->close();
+}
